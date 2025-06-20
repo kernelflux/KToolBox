e750052ddad3b6350472cf6768eb6205d1d90830
kernelflux <ktappdev2024@gmail.com>
2025-06-20 09:30:56 +0800

    init project

---
+++ ktoolbox/src/main/java/com/kernelflux/ktoolbox/HandlerUtils.kt
@@ -0,0 +1,40 @@
+package com.kernelflux.ktoolbox
+
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+
+object HandlerUtils {
+    private val mUiHandler: Handler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            (msg.obj as? Runnable)?.run()
+            super.handleMessage(msg)
+        }
+    }
+
+    @JvmStatic
+    fun post(runnable: Runnable?) {
+        runnable?.also {
+            mUiHandler.post(it)
+        }
+    }
+
+    @JvmStatic
+    fun post(runnable: Runnable?, delay: Long) {
+        runnable?.also {
+            mUiHandler.postDelayed(it, delay)
+        }
+    }
+
+    @JvmStatic
+    fun remove(runnable: Runnable?) {
+        runnable?.also {
+            mUiHandler.removeCallbacks(it)
+        }
+    }
+
+    @JvmStatic
+    fun clearMsg() {
+        mUiHandler.removeCallbacksAndMessages(null)
+    }
+}
